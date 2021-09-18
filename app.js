92f53b16cf89ca28020b2f823c74596012b8b411
Sunil Ahuja <sunil.ahuja@akqa.com>
2021-09-18 21:30:15 +0530

    added more packages

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv/config')
 
 const app = express();
 app.use(cors());
@@ -10,6 +11,7 @@ const port = process.env.PORT || 4000;
 
 app.listen(port, () =>{
     console.log(`server started at ${port}`);
+    console.log(process.env.DB_CONNECTION);
 })
 
 // index page
